5ba5d4d11b0c0cfd54455e561d91f90efdfb45ad
Hemit Patel <hemitvpatel@gmail.com>
2025-07-07 14:18:16 -0400

    remove unused comments + add env example (docs) + do not add .env or .env.local

--- services/httpAIHeader.ts
+++ services/httpAIHeader.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-// Create an Axios instance
 const baseURL = import.meta.env.AI_MIRCOSERVICE_URL
 const httpAIHeader = axios.create({
   baseURL, 
-  withCredentials: true, // Ensure cookies are sent with each request (for session handling)
+  withCredentials: true, 
 });
 
 export default httpAIHeader;
